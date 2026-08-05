b63cadee4c9f1278ae512b92d1a8219052f29f06
NikhilMoudgil <159040779+NikhilMoudgil@users.noreply.github.com>
2026-08-05 22:56:20 +0530

    Basic COmplte

---
+++ 03Functions/05_JsEC.js
@@ -0,0 +1,14 @@
+//JavaScript Execution context 
+function one(){
+    console.log("One");
+}
+function two(){
+    console.log("Two");
+}
+function three(){
+    console.log("Three");
+}
+
+one()
+two()
+three()
