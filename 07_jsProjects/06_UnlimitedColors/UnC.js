a3684cce4faa9538fdeba80f1f6a2c38b0399d72
NikhilMoudgil <nikhilmoudgil799@gmail.com>
2026-08-12 22:03:22 +0530

    projects

---
+++ 07_jsProjects/06_UnlimitedColors/UnC.js
@@ -0,0 +1,27 @@
+// Generate a random color
+const randomColor = function () {
+  const hex = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += hex[Math.floor(Math.random() * 16)];
+  }
+  return color;
+};
+let intervalID;
+const startChangingColor = function () {
+  if (!intervalID) {
+    intervalID = setInterval(changebgcolor, 1000);
+  }
+
+  function changebgcolor() {
+    document.body.style.backgroundColor = randomColor();
+  }
+};
+const stopChangingColor = function () {
+  clearInterval(intervalID);
+  intervalID = null;
+  console.log('Changing color Stopped');
+};
+document.querySelector('#start').addEventListener('click', startChangingColor);
+
+document.querySelector('#stop').addEventListener('click', stopChangingColor);
